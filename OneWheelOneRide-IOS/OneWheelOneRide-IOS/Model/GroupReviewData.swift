3dc2184b34e1cb271a6c32e4c664a4650489f494
Sunder96u <stevenunderwood2@hotmail.com>
2023-07-31 16:43:10 -0500

    update?

--- OneWheelOneRide-IOS/OneWheelOneRide-IOS/Model/GroupReviewData.swift
+++ OneWheelOneRide-IOS/OneWheelOneRide-IOS/Model/GroupReviewData.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+
+struct GroupReview: Codable, Identifiable {
+    var id: Int
+    var comment: String
+    var user_id: Int
+    var group_id: Int
+}
+
+class GroupReviewData: ObservableObject{
+    @Published var groupReview = [GroupReview]()
+    
+    func loadData(completion:@escaping ([GroupReview]) -> ()) {
+        guard let url = URL(string: "https://onewheeloneride-back.up.railway.app/comments") else {
+            print("Invalid url...")
+            return
+        }
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            let groupReview = try! JSONDecoder().decode([GroupReview].self, from: data!)
+            print(groupReview)
+            DispatchQueue.main.async {
+                completion(groupReview)
+            }
+        }.resume()
+    }
+}
